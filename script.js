135352ea9af9ed61299188e21093d9a6ca6dd82d
captainmoon347-stack <captainmoon347@gmail.com>
2026-08-01 12:00:01 +0530

    Update website style

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Modern SMP Website Loaded");
+body{
+    margin:0;
+    padding:0;
+    background:#0d1117;
+    color:white;
+    font-family:Arial,sans-serif;
+    text-align:center;
+}
+
+h1{
+    margin-top:100px;
+    font-size:40px;
+    color:#00bfff;
+}
+
+p{
+    font-size:20px;
+    color:#d0d0d0;
+}
