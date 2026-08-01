41f526e9a14630391407ccb1cad1d331059ce7e9
captainmoon347-stack <captainmoon347@gmail.com>
2026-08-01 12:05:10 +0530

    Style buttons

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 console.log("Modern SMP Website Loaded!");
 
 alert("Welcome to Modern SMP Official Website!");
+button{
+    background:#00bfff;
+    color:white;
+    border:none;
+    padding:15px 25px;
+    margin:10px;
+    font-size:18px;
+    border-radius:10px;
+    cursor:pointer;
+    transition:0.3s;
+}
+
+button:hover{
+    background:#0095cc;
+    transform:scale(1.08);
+}
